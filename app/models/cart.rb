f9bd30b37084681d5ccbeae9c978ffa7a5eee350
Eddie Kao <eddie@digik.com.tw>
2019-06-03 12:11:21 +0800

    add CartItem model and tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,18 @@
 class Cart
+  attr_reader :items
+
   def initialize
     @items = []
   end
 
   def add_item(product_id)
-    @items << product_id
+    # 商品是否存在
+    found_item = @items.find { |item| item.product_id == product_id}
+    if found_item
+      found_item.increment
+    else
+      @items << CartItem.new(product_id)
+    end
   end
 
   def empty?
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  it "可以把商品丟到到購物車裡，然後購物車裡就有東西了。" do
-    cart = Cart.new
-    cart.add_item 1
-    expect(cart.empty?).to be false
+  describe "基本功能" do
+    it "可以把商品丟到到購物車裡，然後購物車裡就有東西了。" do
+      cart = Cart.new
+      cart.add_item 1
+      expect(cart.empty?).to be false
+    end
+
+    it "加了相同種類商品到購物車，購買項目（CartItem）不會增加，商品的數量會改變。" do
+      cart = Cart.new
+
+      3.times { cart.add_item 1 }
+      2.times { cart.add_item 2 }
+      3.times { cart.add_item 1 }
+
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.quantity).to be 6
+    end
+  end
+
+  describe "進階功能" do
   end
 end
