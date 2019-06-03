0b1edd28b26de5d9f21e31b9640a5837efb87b14
Eddie Kao <eddie@digik.com.tw>
2019-06-03 12:39:02 +0800

    use let

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  let(:cart) { Cart.new }
+
   describe "基本功能" do
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了。" do
-      cart = Cart.new
       cart.add_item 1
       expect(cart.empty?).to be false
     end
 
     it "加了相同種類商品到購物車，購買項目（CartItem）不會增加，商品的數量會改變。" do
-      cart = Cart.new
-
       3.times { cart.add_item 1 }
       2.times { cart.add_item 2 }
       3.times { cart.add_item 1 }
