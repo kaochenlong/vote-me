d0a72b36808a8837f459c88bde63593a694b37ba
Eddie Kao <eddie@digik.com.tw>
2019-06-05 12:13:15 +0800

    add scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
   # scope
   scope :available, -> { where(is_available: true) }
-  scope :expensive_product, -> { where('price >= 50') }
+  scope :expensive_product, -> { price_over(50) }
   scope :good_product, -> { available.expensive_product }
+  scope :price_over, -> (n) { where("price >= #{n}") }
 
   # def self.available
   #   where(is_available: true)
