d5555de2c4c2a69ad329a2f200b3c452a892980f
Eddie Kao <eddie@digik.com.tw>
2019-06-06 14:12:14 +0800

    order created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,33 @@
 class OrdersController < ApplicationController
+  include CartsHelper
+
+  before_action :authenticate_user!
+
   def create
     # 建立訂單
+    # @order = Order.new(order_params)
+    # @order.user = current_user
 
-    # 刷卡
-    redirect_to products_path, notice: '親，感謝!'
+    @order = current_user.orders.build(order_params)
+
+    current_cart.items.each do |item|
+      @order.order_items.build(product: item.product,
+                               quantity: item.quantity,
+                               strike_price: item.product.price)
+    end
+
+    if @order.save
+      # 刷卡
+      # 清空購物車
+      redirect_to products_path, notice: '親，感謝!'
+    else
+      render 'carts/checkout'
+    end
+
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:recipient, :phone, :address, :note)
   end
 end
